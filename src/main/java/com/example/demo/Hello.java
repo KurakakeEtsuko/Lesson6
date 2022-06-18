9d20f87a2e732eca5e50aaa53a5211184c1e57d3
KurakakeEtsuko <diva1565@yyahoo.co.jp>
2022-06-18 10:25:41 +0900

    課題6回目2

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,15 +1,28 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Hello {
 
-    @RequestMapping("/hello")
-    public String HelloWorld() {
+    @GetMapping("/hello")
+    public String HelloWorld(Model model) {
+
+	LocalDateTime nowDate = LocalDateTime.now();
+
+	DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒 E曜日");
+
+	String formatNowDate = dtf2.format(nowDate);
+
+	model.addAttribute("message", "本日の日時と曜日をお知らせします。");
+	model.addAttribute("day", formatNowDate);
 
 	return "HelloWorld";
-
     }
+
 }
